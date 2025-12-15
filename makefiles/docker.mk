e9055e5216924368cf2ccf4c13c9ea05402cf20f
John Dunglao <jfsdunglao@gmail.com>
2025-12-15 19:40:54 +0800

    production ready docker containers

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -1,4 +1,4 @@
-ENV?=development
+ENV?=local
 ARGUMENT?=
 
 USE_PROD_ENV_FILES = --env-file ./docker/environments/.env \
@@ -29,14 +29,18 @@ USE_ORIGINAL_COMPOSE = -f ./docker/docker-compose.yml
 USE_LOCAL_COMPOSE = -f ./docker/docker-compose.local.yml
 USE_PROD_COMPOSE = -f ./docker/docker-compose.prod.yml
 
+# to use production environment files and compose
+# set ENV=production when calling make command
+# example: make build ENV=production
 ifeq ($(ENV), production)
 	COMPOSE_COMMAND = docker compose $(USE_PROD_ENV_FILES) $(USE_ORIGINAL_COMPOSE) $(USE_PROD_COMPOSE)
 else
 	COMPOSE_COMMAND = docker compose $(USE_LOCAL_ENV_FILES) $(USE_ORIGINAL_COMPOSE) $(USE_LOCAL_COMPOSE)
 endif
 
+# added . at the end to specify current directory as context
 build:
-	$(COMPOSE_COMMAND) build
+	$(COMPOSE_COMMAND) build .
 
 command:
 	$(COMPOSE_COMMAND) $(ARGUMENT)
