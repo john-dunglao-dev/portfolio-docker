ecee570c3b814efe5efaafa973f98b708b4f4d65
John Dunglao <jfsdunglao@gmail.com>
2025-12-17 14:06:33 +0800

    server setup done for nodejs and reactjs container. fixing env for php laravel side

--- makefiles/git.mk
+++ makefiles/git.mk
@@ -1,12 +1,13 @@
 GITHUB_SHA?=latest
 COMMAND = docker compose
 COMPOSE_FILES = -f ./docker/docker-compose.yml -f ./docker/docker-compose.prod.yml
+BUILD_ARG?=
 
 production-build-php:
-	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build php
+	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build php $(BUILD_ARG)
 
 production-build-nginx:
-	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build nginx
+	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build nginx $(BUILD_ARG)
 
 production-push-php:
 	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) push php
@@ -15,7 +16,7 @@ production-push-nginx:
 	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) push nginx
 
 production-build:
-	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build
+	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) build $(BUILD_ARG)
 
 production-push:
 	GITHUB_SHA=$(GITHUB_SHA) $(COMMAND) $(COMPOSE_FILES) push
